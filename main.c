64fbd91daae37edf226a3aef8a35cd232deb05b6
Håvard Skordal Thorsen <haavardsth@uia.no>
2020-10-10 20:18:06 +0200

    Added the project to git

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char value = 0;
+    printf("This program prints most of the ASCII table!\n\n");
+    for(int i = 1; i < 256; i++){
+        value = i;
+        printf("%i. %c  ", i, value);
+        if (i%10 == 0 && i != 0 && i != 10){
+            printf("\n");
+        }
+    }
+    return 0;
+}
